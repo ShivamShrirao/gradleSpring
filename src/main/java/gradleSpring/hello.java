ad115135a6dbe1848a0f7d0a369a5c5a05e09856
ShivamShrirao <shivamshrirao@gmail.com>
2020-02-26 12:27:46 +0530

    files added

---
+++ src/main/java/gradleSpring/hello.java
@@ -0,0 +1,10 @@
+package gradleSpring;
+
+import org.joda.time.LocalTime;
+
+public class hello {
+    public static void main(String[] args) {
+        LocalTime ctime = new LocalTime();
+        System.out.println("Time is: "+ctime);
+    }
+}

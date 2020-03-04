718a1adeab2af513610b351081b7c38fdfceddd2
Shivam Shrirao <shivamshrirao@gmail.com>
2020-03-04 17:08:44 +0530

    spring code annotations

--- src/main/java/gradleSpring/AppConfig.java
+++ src/main/java/gradleSpring/AppConfig.java
@@ -7,12 +7,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "gradleSpring")
 public class AppConfig {
-//    @Bean
-//    public Samsung getPhone(){
-//        return new Samsung();
-//    }
-//    @Bean
-//    public MobileProcessor getProcessor(){
-//        return new SnapDragon();
-//    }
 }
